ee60a4023ab5c58533be42449957e0da0eee0495
Joey Saha <joeysaha@hotmail.com>
2018-07-24 12:57:06 -0400

    finishing up paperboy game

--- exercise4paperboy.rb
+++ exercise4paperboy.rb
@@ -29,41 +29,49 @@ class Player
     @quota
   end
 
-  def work(x,y)
-    @exp += 1
-    if @exp < quota
-      @earnings += 0.25
-    else
-      @earnings += 0.50
+  def deliver(x, y)
+    m=0
+    n=y-x
+    while m<n
+      @exp += 1
+      if @exp < quota
+        @earnings += 0.25
+      else
+        @earnings += 0.50
+      end
+    m += 1
+    end
+    if n<@quota
+      @earnings -= 2
     end
   end
 
   def quota
-    @quota = 50+@exp/2
+    @quota = 50.0+@exp/2.0
   end
 
-  def end_game
-    @quota = 50
+  def report
+    puts "#{@name} has #{@exp} houses of
+    experience and has earned $#{@earnings}. His
+    quota to meet is #{@quota} houses."
   end
-
-
-
-
-
 end
 
 boy = Player.new("Dude")
 
-def output(boy)
-  boy=boy
-  puts "#{boy.name} has #{boy.exp} and has earned
-  #{boy.earnings}. His quota to meet is
-  #{boy.quota}"
-end
+# def output(boy)
+#   puts "#{boy.name} has #{boy.exp} houses of
+#   experience and has earned $#{boy.earnings}. His
+#   quota to meet is #{boy.quota} houses."
+# end
 
 # Player.name = "Dude"
-puts boy.name
-puts boy.exp
-puts boy.earnings
-puts boy.quota
-puts output(boy)
+# puts boy.name
+# puts boy.exp
+# puts boy.earnings
+# puts boy.quota
+# puts output(boy)
+# boy.deliver(1, 102)
+# puts output(boy)
+boy.deliver(1,102)
+boy.report
